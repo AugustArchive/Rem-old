2a7fb27ca534f11fec76f68feca9e2599599eead
August <view92gaming@gmail.com>
2017-09-16 18:37:11 +0000

    v1.0.1

--- src/Rem.js
+++ src/Rem.js
@@ -4,24 +4,42 @@ let bot = new Client({ disableEveryone: true, autoReconnect: true });
 
 bot.login(config.api_keys.Discord);
 
-bot.on('ready', () => {
+function setGame() {
+  let games = [
+    "With August! OwO",
+    `On ${bot.guilds.size} guilds!`,
+    "With a ball of yarn!",
+    "With Aqua-sama",
+    "With Mio-chan",
+    "Facing Ayana",
+    "With my Lucario plush!",
+    "With my Umbreon plushie!"
+  ];
   bot.user.setPresence({
-   game: {
-     name: config.prefix + "help | [" + bot.guilds.size + "] | " + config.prefix + "stats",
-     type: 0
-   }
+    status: 'online',
+    afk: 'false',
+    game: {
+      url: 'https://twitch.tv/discordapp',
+      name: `r/help | ${games[Math.floor(Math.random() * games.length)]} [${bot.guilds.size}]`,
+      type: 0
+    }
   });
+}
+
+bot.on('ready', () => {
+  setGame();
+  bot.setInterval(setGame, 50000);
   console.log(`[READY] ${bot.user.username} is ready!`);
 });
 
 bot.on('guildCreate', (guild) => {
-  bot.channels.get('355938690322661376').send(`🆕 **|** Joined a new guild!\n\`\`\`asciidoc\n= Guild Infomation =\nGuild Name (ID)     :: ${guild.name} (${guild.id})\`\`\``);
+  bot.channels.get('358052869515116548').send(`🆕 **|** Joined a new guild!\n\`\`\`asciidoc\n= Guild Infomation =\nGuild Name (ID)     :: ${guild.name} (${guild.id})\`\`\``);
   console.log(`[LEFTED GUILD] Rem joined a new guild!\n${guild.name} (${guild.id})`);
   // postServerStats();
 });
 
 bot.on('guildDelete', (guild) => {
-  bot.channels.get('355938690322661376').send(`:x: **|** Left a new guild!\n\`\`\`asciidoc\n= Guild Infomation =\nGuild Name (ID)     :: ${guild.name} (${guild.id})\`\`\``);
+  bot.channels.get('358052869515116548').send(`:x: **|** Left a new guild!\n\`\`\`asciidoc\n= Guild Infomation =\nGuild Name (ID)     :: ${guild.name} (${guild.id})\`\`\``);
   console.log(`[LEFTED GUILD] Rem lefted a new guild!\n${guild.name} (${guild.id})`);
   // postServerStats();
 });
