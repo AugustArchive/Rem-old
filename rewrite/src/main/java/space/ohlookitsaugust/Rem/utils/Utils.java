1edf98222caac141296a6d90cc56b3e275481ea5
August <view92gaming@gmail.com>
2017-11-04 03:44:03 +0000

    Add new file

---
+++ rewrite/src/main/java/space/ohlookitsaugust/Rem/utils/Utils.java
@@ -0,0 +1,7 @@
+package space.ohlookitsaugust.Rem.utils;
+
+public class Utils {
+  public static final String VERSION = "v2.0.1";
+  private static final String RESTART = "50";
+  public static final String EMBEDFOOTER = "Rem, a discord bot.";
+}
