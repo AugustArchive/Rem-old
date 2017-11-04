b64b18bf5c1ec2b1ead46d29eb9949d55ab1f127
August <view92gaming@gmail.com>
2017-11-04 03:55:28 +0000

    Add new file

---
+++ rewrite/src/main/java/space/ohlookitsaugust/Rem/commands/HelpCommand.java
@@ -0,0 +1,26 @@
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import space.ohlookitsAugust.Rem.handlers.Command;
+
+import java.awt.*;
+import java.util.List;
+
+import static space.ohlookitsAugust.Rem.RemBoat.cats;
+
+
+public class HelpCommand extends Command {
+
+    public HelpCommand(String name, String help, Category category, boolean usableInDM) {
+        super(name, help, category, usableInDM);
+    }
+
+    @Override
+    public void execute(List<String> arguments, MessageReceivedEvent event) {
+        EmbedBuilder builder = new EmbedBuilder()
+                .setAuthor("Rem Command List", null, event.getAuthor().getEffectiveAvatarUrl())
+                .setColor(Color.GREEN);
+        cats.getCommands().forEach(command -> builder.appendDescription("+ **" +
+                cats.getPrefix() + command.getName() + "**: *" + command.getHelp() + "*\n"));
+        event.getChannel().sendMessage(builder.build()).queue();
+    }
+}
