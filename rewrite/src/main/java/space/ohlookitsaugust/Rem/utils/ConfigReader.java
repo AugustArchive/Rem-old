816857ac9cc6533e4eab678aa833ed9f9204f258
August <view92gaming@gmail.com>
2017-11-04 03:39:06 +0000

    Add new file

---
+++ rewrite/src/main/java/space/ohlookitsaugust/Rem/utils/ConfigReader.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+
+public class ConfigReader {
+    private HashMap<String, String> keyPairs = new HashMap<>();
+
+    public ConfigReader(String path, String seperator) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(path));
+        lines.forEach(line -> {
+            String[] split = line.split(seperator);
+            if (split.length == 2) keyPairs.put(split[0], split[1]);
+        });
+    }
+
+    public String get(String key) {
+        return keyPairs.get(key);
+    }
+}
