33d7a71fe2a7443b4cf928afa8825a9c91d1c74b
August <view92gaming@gmail.com>
2017-11-05 10:31:53 +0000

    ~~no~~

---
+++ rewrite/src/main/java/space/ohlookitsaugust/Rem/commands/PingCommand.java
@@ -0,0 +1,16 @@
+package space.ohlookitsaugust.Rem.commands;
+
+import space.ohlookitsaugust.Rem.handlers.Command;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import java.util.List;
+
+public class PingCommand extends Command {
+  public PingCommand(String name, String help, Category category, boolean usableInDM) {
+    super(name, help, category, usableInDM);
+  }
+  
+  @Override
+  public void execute(List<String> arguments, MessageReceivedEvent event) {
+    event.getChannel().sendMessage(Math.round(event.getJda().getPing() + "ms").queue();
+  }
+}
