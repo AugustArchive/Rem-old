d58c4d741dc34fb18e9a5233946c5511f07ec162
August <view92gaming@gmail.com>
2017-11-05 21:38:58 +0000

    FORGOT TO ADD A SEMI-COLON REEE

--- rewrite/src/main/java/space/ohlookitsaugust/Rem/RemBoat.java
+++ rewrite/src/main/java/space/ohlookitsaugust/Rem/RemBoat.java
@@ -8,7 +8,7 @@ import net.dv8tion.jda.core.entities.Game;
 import net.dv8tion.jda.core.hooks.AnnotatedEventManager;
 import com.github.natanbc.discordbotsapi.DiscordBotsAPI;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory
+import org.slf4j.LoggerFactory;
 
 import space.ohlookitsaugust.Rem.commands.HelpCommand;
 import space.ohlookitsaugust.Rem.handlers.Command;
