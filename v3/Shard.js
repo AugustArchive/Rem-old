0dad47a256c2dcad8b49937a90a3bd795942aaf0
August <view92gaming@gmail.com>
2017-11-17 01:20:44 +0000

    Add new file

---
+++ v3/Shard.js
@@ -0,0 +1,10 @@
+const { ShardingManager: RemShard } = require('discord.js');
+const config = require('./config.json');
+const Shard = new RemShard(`${__dirname}/Rem.js`, {
+  token: config.api_keys.Discord,
+  maxShards: "auto"
+});
+
+Shard.spawn();
+
+Shard.on('launch', (s) => console.log(`[SHARD]: Shard ${s.id} launched.`));
